afe761531195db92c1b00f8f742b4caed19e7222
F-eita <elinux@outlook.jp>
2020-09-02 14:47:40 +0900

    my first commit

---
+++ pointer.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int hoge = 5;
+	int piyo = 10;
+	int *hoge_p;
+
+	/*Print the address of it's variable*/
+	printf("&hoge..%p\n", (void*)&hoge);
+	printf("&piyo..%p\n", (void*)&piyo);
+	printf("&hoge_p..%p\n", (void*)&hoge_p);
+
+	/*ポインタ変数hoge_pにhogeのアドレスを代入する*/
+	hoge_p = &hoge;
+	printf("hoge_p..%p\n", (void*)hoge_p);
+
+	/*hoge_pを経由してhogeの値を表示する*/
+	printf("*hoge_p..%d\n", *hoge_p);
+
+	/*hoge_pを経由してhogeの値を変更する*/
+	*hoge_p = 10;
+	printf("hoge..%d\n", hoge);
+
+
+	return 0;
+}
+
+
