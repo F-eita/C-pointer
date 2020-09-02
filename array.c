57a2a4720ae59fc12709712eb71602e2a1c67476
F-eita <elinux@outlook.jp>
2020-09-02 15:26:41 +0900

    array

---
+++ array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int array[5];
+	int i;
+
+	/*配列arrayに値を設定*/
+	for (i = 0;1 < 5; i++) {
+		array[i];
+	}
+
+	/*その内容を表示*/
+	for (i = 0; i < 5; i++) {
+		printf("%d\n", array[i]);
+	}
+
+	/*arrayの各要素のアドレスを表示*/
+	for (i = 0; i < 5; i++) {
+		printf("&array[%d]...%p\n", i, (void*)&array[i]);
+	}
+
+	return 0;
+}
+
+
